0dbaa7296dee093061c285a327e607637c334834
1342130571 <121421857+1342130571@users.noreply.github.com>
2023-08-18 14:46:56 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'test/master'.
    
    Changes to be committed:
    	new file:   learnvuex/.babelrc
    	new file:   learnvuex/.editorconfig
    	new file:   learnvuex/.gitignore
    	new file:   learnvuex/.postcssrc.js
    	new file:   learnvuex/README.md
    	new file:   learnvuex/build/build.js
    	new file:   learnvuex/build/check-versions.js
    	new file:   learnvuex/build/logo.png
    	new file:   learnvuex/build/utils.js
    	new file:   learnvuex/build/vue-loader.conf.js
    	new file:   learnvuex/build/webpack.base.conf.js
    	new file:   learnvuex/build/webpack.dev.conf.js
    	new file:   learnvuex/build/webpack.prod.conf.js
    	new file:   learnvuex/config/dev.env.js
    	new file:   learnvuex/config/index.js
    	new file:   learnvuex/config/prod.env.js
    	new file:   learnvuex/index.html
    	new file:   learnvuex/package-lock.json
    	new file:   learnvuex/package.json
    	new file:   learnvuex/src/App.vue
    	new file:   learnvuex/src/assets/logo.png
    	new file:   learnvuex/src/main.js
    	new file:   learnvuex/static/.gitkeep

---
+++ learnvuex/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   {{ message }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: '我是APP组件'
+    }
+  }
+  }
+
+</script>
+
+<style>
+
+</style>
