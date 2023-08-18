b1bf421d9691372cf72e6f067c1876f43ad46799
1342130571 <121421857+1342130571@users.noreply.github.com>
2023-08-18 15:44:48 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'test/master'.
    
    Changes to be committed:
    	modified:   learnvuex/package-lock.json
    	modified:   learnvuex/package.json
    	modified:   learnvuex/src/App.vue
    	new file:   learnvuex/src/components/HelloVuex.vue
    	modified:   learnvuex/src/main.js
    	new file:   learnvuex/src/store/index.js

--- learnvuex/src/App.vue
+++ learnvuex/src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="app">
-   {{ message }}
+    <h2>{{ message }}</h2>
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="counter++">+1</button>
+    <button @click="counter--">-1</button>
+
+    <hello-vuex></hello-vuex>
   </div>
 </template>
 
 <script>
+import HelloVuex from './components/HelloVuex.vue';
 
 export default {
   name: 'App',
+  components: {
+    HelloVuex
+  },
   data() {
     return {
-      message: '我是APP组件'
+      message: '我是APP组件',
+      counter: 0
     }
   }
-  }
+}
 
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ learnvuex/src/components/HelloVuex.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2>{{ $store.state.counter }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'HelloVuex'
+}
+</script>
+
+<style></style>
