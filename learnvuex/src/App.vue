b361454b79654173a1fed28335536778c1e39c6e
1342130571 <121421857+1342130571@users.noreply.github.com>
2023-08-19 11:58:49 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'test/master'.
    
    Changes to be committed:
    	modified:   learnvuex/src/App.vue
    	modified:   learnvuex/src/store/index.js
    	new file:   learnvuex/src/store/mutations-types.js

--- learnvuex/src/App.vue
+++ learnvuex/src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
     <h2>{{ message }}</h2>
+
+    <h2>-----APP内容：getters相关内容-------</h2>
+    <h2>{{ $store.state.info }}</h2>
+    <button @click="updataInfo">修改信息</button>
+
     <h2>-----APP内容-------</h2>
     <h2>{{ $store.state.counter }}</h2>
-    <button @click="addition()">+1</button>
-    <button @click="subtraction()">-1</button>
+    <button @click="addition">+1</button>
+    <button @click="subtraction">-1</button>
+    <button @click="addCount(5)">+5</button>
+    <button @click="addCount(10)">+10</button>
+    <button @click="addStudnet">添加学生</button>
 
     <h2>-----APP内容：getters相关内容-------</h2>
     <h2>{{ $store.getters.powerCounter }}</h2>
@@ -20,6 +28,7 @@
 
 <script>
 import HelloVuex from './components/HelloVuex';
+import { INCREMENT } from './store/mutations-types';
 
 export default {
   name: 'App',
@@ -33,14 +42,42 @@ export default {
   },
   methods: {
     addition() {
-      this.$store.commit('increment')
+      this.$store.commit(INCREMENT)
     },
     subtraction() {
       this.$store.commit('decrement')
+    },
+    addCount(count) {
+      // payload：负载（提交的时候帮助附带一个东西过去 这个东西称作payload）
+      // 简单的提交封装
+      // this.$store.commit('incremmentCount', count)
+
+      // 复杂的提交封装
+      this.$store.commit({
+        type: 'incremmentCount',
+        count
+      })
+    },
+    addStudnet() {
+      const sub = {id: 114, name: '金刚' , age: 88}
+      this.$store.commit('addStudnet', sub)
+    },
+    updataInfo(){
+      // this.$store.commit('updataInfo')
+      // this.$store.dispatch('aUpdateInfo', {
+      //   message: '我是携带的信息',
+      //   success: () => {
+      //     console.log('完成了');
+      //   }
+      // })
+      this.$store.dispatch('aUpdateInfo', '我是携带的信息')
+      .then(res => {
+        console.log('里面完成了提交');
+        console.log(res);
+      })
     }
   }
 }
-
 </script>
 
 <style></style>
--- learnvuex/src/store/index.js
+++ learnvuex/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import {INCREMENT} from './mutations-types'
 // 1.安装插件
 Vue.use(Vuex)
 
@@ -14,21 +15,65 @@ const store = new Vuex.Store({
       {id: 112 , name: '王五' , age: 19},
       {id: 113 , name: '赵七' , age: 21},
       {id: 114 , name: '李九' , age: 36}
-    ]
+    ],
+    info: {
+      name: '李四',
+      age: 18,
+      height: 1.98
+    }
   },
+  // 状态更新
   mutations: {
     // 方法
-    increment(state) {
+    [INCREMENT](state) {
       state.counter++
     },
     decrement(state) {
       state.counter--
+    },
+    incremmentCount(state, payload){
+      // console.log(payload);
+      state.counter += payload.count
+    },
+    addStudnet(state, sub) {
+      state.students.push(sub)
+    },
+    updateInfo(state){
+      state.info.name = 'coderwhy' //是响应式的
+
+      // 错误的写法，异步操作没有经过actions
+      // setTimeout(() => {
+      //   state.info.name = 'coderwhy'
+      // }, 1000)
+        
+      // state.info['address'] = '洛杉矶' 不是响应式的
+      // Vue.set(state.info, 'address' , '洛杉矶') // 是响应式的
+      // 该方法做不到响应式
+      // delete state.info.age // 不是响应式的
+      // Vue.delete(state.info, 'age') // 是响应式的
     }
   },
   actions: {
+    // context：上下文
+    // aUpdateInfo(context, payload){
+    //   setTimeout(() => {
+    //     context.commit('updateInfo')
+    //     console.log(payload.message);
+    //     payload.success()
+    //   }, 1000)
+    // }
+    aUpdateInfo(context, payload){
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          context.commit('updateInfo')
+          console.log(payload);
 
+          resolve('111111');
+        }, 1000)
+      }) 
+    }
   },
-  // 计算属性
+  // 函数
   getters: {
     powerCounter(state) {
       return state.counter * state.counter
